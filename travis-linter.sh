557302c0bdb7000ac77bdcc03510aafb4b541548
Stefan Knorr <sknorr@suse.de>
2020-01-17 14:40:37 +0100

    Travis gitlint: Allow comparing to branch that is not repo default (noref)
    Travis tries to clone the smallest part of the repo possible. This means
    it clones only the default branch by default and then gets your PR's
    branch.
    
    To compare the commit list, we may need an additional branches however,
    for example, in my case, I needed `maintenance/cloud_9` too. So we now
    fetch that and compare then.
    
    Also add a travis_fold function, to make sure the actual gitlint
    messages are more visible. `set -x -u` needed to go to make travis_fold
    work. Sorry.

--- travis-linter.sh
+++ travis-linter.sh
@@ -1,8 +1,54 @@
 #!/bin/bash
 
-set -x -u
+compare_to="master"
+# Branch parameter from Travis, the branch we want to merge into
+[[ -n "$TRAVIS_BRANCH" ]] && compare_to="$TRAVIS_BRANCH"
+
+
+log() {
+  # $1 - message
+  format="\e[34m"
+  reset='\e[0m'
+  echo -e "$format${1}$reset"
+}
+
+travis_fold_ids=''
+travis_fold() {
+  # $1 - human-readable name for folded section; or '-' as section end marker
+  type='start'
+  humanname="$1"
+  id="fold_"$(( ( RANDOM % 32000 ) + 1 ))
+  if [[ "$1" == '-' ]]; then
+    type='end'
+    humanname=''
+    id=$(echo "$travis_fold_ids" | cut -d' ' -f'1')
+    travis_fold_ids=$(echo "$travis_fold_ids" | cut -d' ' -f'2-')
+  else
+    travis_fold_ids="$id $travis_fold_ids"
+  fi
+  [[ $type == 'start' ]] && echo ""
+  echo -en "travis_fold:$type:$id\\r"
+  log "$humanname"
+}
+
+
+travis_fold "Setting Up Tooling"
 
 pip install gitlint
 wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/gitlint.ini
 wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/extra-gitlint-rules.py
-gitlint --commits master..HEAD -C gitlint.ini
+
+travis_fold -
+
+
+travis_fold "Checking out comparison branch $compare_to"
+
+git checkout -b temp-pr_state_branch
+
+git fetch origin $compare_to
+git checkout -b temp-comparison_branch FETCH_HEAD
+
+travis_fold -
+
+
+gitlint --commits temp-comparison_branch..temp-pr_state_branch -C gitlint.ini
