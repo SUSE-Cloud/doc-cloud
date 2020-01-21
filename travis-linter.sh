7362801b81ce5b1bf91fad82402ed8c70ad72e10
Nicolas Bock <nicolas.bock@suse.com>
2020-01-21 13:32:39 -0700

    Fix linter git references (bug)

--- travis-linter.sh
+++ travis-linter.sh
@@ -2,6 +2,12 @@
 
 set -x -u
 
+git rev-list -h
+git rev-list master | head -n5
+git rev-list HEAD | head -n5
+git rev-list master..HEAD
+git branch
+
 pip install gitlint
 wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/gitlint.ini
 wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/extra-gitlint-rules.py
