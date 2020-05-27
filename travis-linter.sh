1609adcee544b6701c294745930fbc29463949e3
Stefan Knorr <sknorr@suse.de>
2020-05-27 23:20:27 +0200

    Remove CI setup
    We don't expect to build anything useful on this branch ever again,
    so we can probably just remove the CI setup here.

--- travis-linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x -u
-
-pip install gitlint
-wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/gitlint.ini
-wget https://raw.githubusercontent.com/openSUSE/doc-ci/develop/gitlint/extra-gitlint-rules.py
-gitlint --commits master..HEAD -C gitlint.ini
